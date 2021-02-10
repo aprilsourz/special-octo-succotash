a0200711f94585d5d21a5c01731a866966df5ea8
Arlo Feirman <arlofeirman@gmail.com>
2021-02-10 14:56:52 -0700

    Multi stage build

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+    "github.com/arlo-feirman.special-octo-succotash/database"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run(":3000")
+	r.GET("/animal/:name", func(c *gin.Context) {
+        animal, err := database.GetAnimal(c.Param("name"))
+        if err != nil {
+          c.String(404, err.Error())
+          return
+        }
+        c.JSON(200, animal)
+      })
 }
+
+
